a281795f66f08ad3bec3b519ddb7bfa4c28c8abf
ilkaysefer <114875773+ilkaysefer@users.noreply.github.com>
2023-01-04 21:14:29 +0300

    Update listFlatten.py

--- listFlatten.py
+++ listFlatten.py
@@ -1,16 +1,3 @@
-# list = [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
-# fList = [i for element in list for i in element]
-# print(l)
-# print(fList)
-
-# list = [[1,3,"geeks"], [4,5],[6,"best"]]
-# res = [i for row in list for i in row]
-# print(res)
-
-# l= [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
-# res = [i for row in list for i in row]
-# print(res)
-
 l = [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
 
 def flattenList(main_list):
